94bf298a44c3dc4d86d32fb4a00c211175a47859
Михаил <muxazila@mail.ru>
2023-09-11 11:55:24 +0700

    Загрузка файлов

--- download_files/views.py
+++ download_files/views.py
@@ -27,6 +27,7 @@ class DownloadFiles(LoginRequiredMixin, FormView):
                 first_name = row['first_name']
                 last_name = row['last_name']
                 role_name = row['role_name']
+                password = row['password']
 
                 # Проверяем, существует ли пользователь по username
                 user, created = CustomUser.objects.get_or_create(username=username)
@@ -34,6 +35,7 @@ class DownloadFiles(LoginRequiredMixin, FormView):
                 # Обновляем поля пользователя
                 user.first_name = first_name
                 user.last_name = last_name
+                user.set_password(password)
 
                 # Находим или создаем роль
                 role, created = Role.objects.get_or_create(name=role_name)
