815255436f231be5511f2730a25209b8eb61357f
MikhailFrushkin <muxazila@mail.ru>
2023-12-18 13:01:51 +0700

    Загрузка фото нужных форматов и сжатие

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,13 @@
+from io import BytesIO
+
+from PIL import Image
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.core.exceptions import ValidationError
+from django.core.files.images import ImageFile
+from loguru import logger
+
 from .models import CustomUser, Role
 from django.utils.translation import gettext_lazy as _
 
@@ -63,3 +70,27 @@ class UserProfileEditForm(forms.ModelForm):
         if self.instance.birth_date:
             birth_date_str = self.instance.birth_date.strftime('%Y-%m-%d')
             self.initial['birth_date'] = birth_date_str
+
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo')
+
+        if photo:
+            # Проверка расширения файла
+            allowed_extensions = ['png', 'jpg', 'jpeg']
+            extension = str(photo.name.split('.')[-1]).lower()
+
+            if extension not in allowed_extensions:
+                raise ValidationError('Пожалуйста, загрузите изображение в формате png, jpg или jpeg.')
+
+            # Проверка размера изображения (не обязательная часть)
+            max_size = (1024, 1024)  # Пример: максимальный размер 1024x1024
+            image = Image.open(photo)
+            image.thumbnail(max_size)
+            output = BytesIO()
+            image.save(output, format=image.format)
+            output.seek(0)
+
+            # Создание нового объекта ImageFieldFile и присвоение его полю photo
+            photo = ImageFile(output, name=f"{self.instance.pk}_profile_picture.{extension}")
+
+        return photo
